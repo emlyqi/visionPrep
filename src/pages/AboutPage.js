07a539e40721f790b455966ae6d00c06d13956d9
Emily Qi <emilyqi2019@gmail.com>
2023-12-26 00:47:06 -0500

    added text to about pg

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,11 +3,13 @@ import Navbar from '../components/Navbar'
 
 export class AboutPage extends PureComponent {
   render() {
+    const aboutText = "This is information about ‘visionPrep."
+
     return (
-      <div>
+      <div style={{height: '100vh', backgroundColor: '#34363D'}}>
         <Navbar></Navbar>
-        <p>
-            hi
+        <p style={{position: 'absolute', left:'50%', top:'50%', transform: 'translate(-50%,-50%)', color: 'white'}}>
+          {aboutText}
         </p>
       </div>
     )
