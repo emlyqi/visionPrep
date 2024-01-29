92b89e653a276d3ae64e95d1195db87e24c24af6
JAMIE XIAO <jamie.xiao.ca@gmail.com>
2024-01-29 10:49:10 -0500

    uploadpage grid

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -5,8 +5,17 @@ import { Grid } from "@mui/material";
 function UploadPage() {
     return (  
         <Grid container flexGrow xs={12} minHeight='100vh' bgcolor='#34363D' justifyContent="center" alignItems="center">
-            <Grid item xs={8}>
-
+            <Grid 
+                item 
+                height='83%'
+                container
+                marginTop='5.81rem' 
+                borderRadius='0.9375rem' 
+                justifyContent='center' 
+                alignContent='center' 
+                backgroundColor='#26272B'
+            >
+                
             </Grid>
         </Grid>
     );
