0c3caabd9a4a696fd9cf2edc458ec84e870ce23a
JAMIE XIAO <jamie.xiao.ca@gmail.com>
2024-02-01 08:47:47 -0500

    bg of upload

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -7,15 +7,14 @@ function UploadPage() {
         <Grid container flexGrow xs={12} minHeight='100vh' bgcolor='#34363D' justifyContent="center" alignItems="center">
             <Grid 
                 item 
-                height='83%'
                 container
                 marginTop='5.81rem' 
                 borderRadius='0.9375rem' 
                 justifyContent='center' 
                 alignContent='center' 
                 backgroundColor='#26272B'
+                style={{width: "79.4375rem", height: "53.125rem"}}
             >
-                
             </Grid>
         </Grid>
     );
