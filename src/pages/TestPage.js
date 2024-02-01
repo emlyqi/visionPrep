d1b7aacd7ffe9f5bc5e4d8bc1f1836766a8a3f56
JAMIE XIAO <jamie.xiao.ca@gmail.com>
2024-02-01 09:12:12 -0500

    testpage elements

---
+++ src/pages/TestPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Grid } from "@mui/material"; 
+
+
+function TestPage() {
+    return (  
+        <Grid container flexGrow xs={12} minHeight='100vh' bgcolor='#34363D' justifyContent="center" alignItems="center">
+            <Grid 
+                item 
+                container
+                marginTop='1rem' 
+                borderRadius='0.9375rem' 
+                justifyContent='center' 
+                alignContent='center' 
+                backgroundColor='#26272B'
+                style={{width: "83rem", height: "42rem"}}
+            >
+            </Grid>
+        </Grid>
+    );
+}
+
+export default TestPage;
