3167b2a51499111cafabf7ae0b8a89a73d820673
Emily Qi <emilyqi2019@gmail.com>
2024-02-13 09:09:10 -0500

    ??

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,8 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import Stack from '@mui/material/Stack';
-
+import BlueLogo from '../assets/visionprep logo blue.png';
+import WhiteLogo from '../assets/visionprep logo blue.png';
 
 function Navbar(props) {
     const [mobileOpen, setMobileOpen] = React.useState(false);
