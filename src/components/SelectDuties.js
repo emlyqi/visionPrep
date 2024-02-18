1995d09b1d1f8370a407c3a7eee05837e79c18e8
Emily Qi <emilyqi2019@gmail.com>
2024-02-18 15:22:43 -0500

    selectduties

---
+++ src/components/SelectDuties.js
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import { Stack, Box } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { SingleInputTimeRangeField } from '@mui/x-date-pickers/SingleInputTimeRangeField';
+
+function SelectDuties () {
+  return(
+    <Box
+      width='100%'
+      height='100%'
+      display='flex'
+      justifyContent='space-evenly'
+      alignItems='flex-start'
+    >
+      <Box 
+        width='40%'
+        justifyContent='flex-start'
+        alignItems='flex-start'
+        flexDirection='column'
+        marginTop='4.25rem'
+      >
+        <p
+          style={{
+            letterSpacing: '0.0625rem',
+            fontFamily: 'sans-serif',
+            verticalAlign: 'center',
+            fontSize: '1.875rem',
+            color: '#5790FF',
+            display: 'inline-block',
+            whiteSpace: 'nowrap',
+            fontWeight: 'bold'             
+          }}>Duty</p>
+        <TextField
+          label='Duty #'
+          sx={{
+            display: 'grid',
+            color: '#5790FF'
+          }}
+        />
+      </Box>
+      <Box
+        width='40%'
+        justifyContent='flex-start'
+        alignItems='flex-start'
+        flexDirection='column'
+        marginTop='4.25rem'
+      >
+        <p
+          style={{
+            letterSpacing: '0.0625rem',
+            fontFamily: 'sans-serif',
+            verticalAlign: 'center',
+            fontSize: '1.875rem',
+            color: '#5790FF',
+            display: 'inline-block',
+            whiteSpace: 'nowrap',
+            fontWeight: 'bold'         
+          }}>Time</p>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <SingleInputTimeRangeField label="From - To"/>
+        </LocalizationProvider>
+      </Box>
+    </Box>
+      
+  );
+}
+
+export default SelectDuties;
