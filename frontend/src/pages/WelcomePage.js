ab7d60ddd9c7d195ef554afc1d472e861fdc71a4
Emily Qi <emilyqi2019@gmail.com>
2024-04-22 00:43:40 -0400

    new welcome pg pic

--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import welcomePic from '../assets/coverpic.png';
+import { Stack, Box, Button } from '@mui/material';
 
 function WelcomePage() {
   
@@ -7,6 +9,14 @@ function WelcomePage() {
     <div style={{ backgroundColor: '#34363D', height: '100vh'}}>
       <Navbar></Navbar>
       <div>Welcome Page</div>
+      <img 
+        src={welcomePic} 
+        alt="emily and jamie make a deal (baby version)" 
+        width="auto"
+        height="80%"
+        margin="auto"
+      />
+
     </div>
   )
 }
